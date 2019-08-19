3353d73e85c02fce847de3912b396e8692422e33
CasparOne <vvolodin.victor@gmail.com>
2019-08-19 18:06:29 +0300

    Added Zend\Diactoros component. All custom classes are deleted

--- public/index.php
+++ public/index.php
@@ -1,23 +1,27 @@
 <?php
 chdir(dirname(__DIR__));
 
-use Framework\Http\RequestFactory;
+
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\ServerRequestFactory;
+
 
 require 'vendor/autoload.php';
 ### Init
-$request = RequestFactory::fromGlobals();
+$request = ServerRequestFactory::fromGlobals();
 
 
 ### Action
 $name = $request->getQueryParams()['name'] ?? 'Guest';
 
-$response = (new \Framework\Http\Response('Hello, ' . $name . '!'))
+$response = (new HtmlResponse('Hello, ' . $name . '!'))
     ->withHeader('X-Developer', 'CasparOne');
 
 ### Send
+
 header('HTTP/1.0' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
-foreach ($response->getHeaders() as $header => $value) {
-    header($header. ':' . $value);
+foreach ($response->getHeaders() as $name => $value) {
+    header($name. ':' . implode(', ', $value));
 }
 
 echo $response->getBody();
