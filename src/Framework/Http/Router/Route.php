9c9925d3795ae38121d180a95a1663c28389726e
CasparOne <vvolodin.victor@gmail.com>
2019-08-22 13:01:04 +0300

    Little optimization. All pasta-code reworked into different class

--- src/Framework/Http/Router/Route.php
+++ src/Framework/Http/Router/Route.php
@@ -3,6 +3,9 @@
 
 namespace Framework\Http\Router;
 
+use InvalidArgumentException;
+use Psr\Http\Message\ServerRequestInterface;
+
 class Route
 {
     public $name;
@@ -19,4 +22,45 @@ class Route
         $this->tokens = $tokens;
         $this->methods = $methods;
     }
+
+    public function match(ServerRequestInterface $request) : ?Result
+    {
+        if ($this->methods && !in_array($request->getMethod(), $this->methods, true)) {
+            return null;
+        }
+        $pattern = preg_replace_callback('~\{([^\}]+)\}~', function ($matches){
+            $argument = $matches[1];
+            $replace = $route->tokens[$argument] ?? '[^}]+';
+            return '(?P<' . $argument . '>' . $replace . ')';
+        }, $this->pattern);
+
+        $path = $request->getUri()->getPath();
+        if (preg_match('~^' . $pattern . '$~i', $path, $matches)) {
+            return new Result(
+                $this->name,
+                $this->handler,
+                array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY));}
+        return null;
+    }
+
+    public function generate($name, array $params = []) : ?string
+    {
+        $arguments = array_filter($params);
+        if ($name !== $this->name) {
+            return null;
+        }
+        $url = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use (&$arguments) {
+            $argument = $matches[1];
+            if (!array_key_exists($argument, $arguments)) {
+                throw new InvalidArgumentException('Missing parameter "' . $argument . '"');
+            }
+            return $arguments[$argument];
+        }, $this->pattern);
+        if ($url !== null) {
+            return $url;
+        }
+        return null;
+    }
+
+
 }
