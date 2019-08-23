7199d6fa61d17d7eaa123dc00641d64cd1d18f8d
CasparOne <vvolodin.victor@gmail.com>
2019-08-23 10:04:10 +0300

    Add coments to strong understanding the material

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ use App\Http\Action\AboutAction;
 use App\Http\Action\Blog\IndexAction;
 use App\Http\Action\Blog\ShowAction;
 use App\Http\Action\HelloAction;
+use Framework\Http\ActionResolver;
 use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Framework\Http\Router\RouteCollection;
 use Framework\Http\Router\Router;
@@ -16,7 +17,9 @@ use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 require 'vendor/autoload.php';
 ### Initialization
-
+/*
+ * Creating route collection
+ */
 $routes = new RouteCollection();
 
 $routes->get('home', '/', HelloAction::class);
@@ -27,25 +30,35 @@ $routes->get('/blog', '/blog', IndexAction::class);
 
 $routes->get('/blog_show', '/blog/{id}', ShowAction::class,['id' => '\d+']);
 
+// Router initialization with previously created RouteCollection
 $router = new Router($routes);
 
+// Creating Resolver with some actions creation logic
+$resolver = new ActionResolver();
+
 ### Running
+//Initialization a new Request object
 $request = ServerRequestFactory::fromGlobals();
 
 try {
+    // Parsing current route
     $result = $router->match($request);
+    // Getting Attributes from parsing result and setting up the Request
     foreach ($result->getAttribute() as $attribute => $value) {
         $request = $request->withAttribute($attribute, $value);
     }
+    // Getting Handler name
     $handler = $result->getHandler();
-    $action = is_string($handler) ? new $handler : $handler;
+    // Setting action
+    $action = $resolver->resolve($handler);
+    // Init Response object to return it
     $response = $action($request);
 } catch (RequestNotMatchedException $exception) {
     $response = new HtmlResponse('Undefined Page', 404);
 }
 
 ### Postprocessing
-
+// Add some headers to response
 $response = $response->withHeader('X-Developer', 'CasparOne');
 
 ### Sending
