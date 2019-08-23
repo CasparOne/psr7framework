6ade2704be686c2ee49dadf6ab88ee44ec3fc365
CasparOne <vvolodin.victor@gmail.com>
2019-08-23 21:31:07 +0300

    Some fixes and preparation for next lesson

--- tests/App/Http/Action/Blog/IndexActionTest.php
+++ tests/App/Http/Action/Blog/IndexActionTest.php
@@ -1,24 +1,21 @@
 <?php
-
-
 namespace Tests\App\Http\Action\Blog;
-
-
 use App\Http\Action\Blog\IndexAction;
 use PHPUnit\Framework\TestCase;
-use Zend\Diactoros\ServerRequest;
-
 class IndexActionTest extends TestCase
 {
-    public function testStatus()
+    public function testSuccess()
     {
         $action = new IndexAction();
-        $request = new ServerRequest();
-
-        $response = $action($request);
-
+        $response = $action();
         self::assertEquals(200, $response->getStatusCode());
-        self::assertJson($response->getBody()->getContents());
+        self::assertJsonStringEqualsJsonString(
+            json_encode([
+                ['id' => 3, 'Title' =>'The third Post'],
+                ['id' => 2, 'Title' =>'The Second Post'],
+                ['id' => 1, 'Title' =>'The first Post'],
+            ]),
+            $response->getBody()->getContents()
+        );
     }
-
 }
--- tests/App/Http/Action/Blog/ShowActionTest.php
+++ tests/App/Http/Action/Blog/ShowActionTest.php
@@ -1,37 +1,29 @@
 <?php
-
-
 namespace Tests\App\Http\Action\Blog;
-
-
 use App\Http\Action\Blog\ShowAction;
 use PHPUnit\Framework\TestCase;
 use Zend\Diactoros\ServerRequest;
-
 class ShowActionTest extends TestCase
 {
-    public function testBlogShow()
+    public function testSuccess()
     {
         $action = new ShowAction();
-        $request = new ServerRequest();
-        $request = $request->withQueryParams(['id' => 1]);
-
+        $request = (new ServerRequest())
+            ->withAttribute('id', $id = 2);
         $response = $action($request);
-
         self::assertEquals(200, $response->getStatusCode());
-        self::assertJson($response->getBody()->getContents());
+        self::assertJsonStringEqualsJsonString(
+            json_encode(['id' => $id, 'title' => 'Post # ' . $id]),
+            $response->getBody()->getContents()
+        );
     }
-
-    public function testError()
+    public function testNotFound()
     {
         $action = new ShowAction();
-        $request = new ServerRequest();
-        $request = $request->withQueryParams(['id' => '88']);
-
+        $request = (new ServerRequest())
+            ->withAttribute('id', $id = 10);
         $response = $action($request);
-
-        self::assertEquals(200, $response->getStatusCode());
-        self::assertJson($response->getBody()->getContents());
+        self::assertEquals(404, $response->getStatusCode());
+        self::assertEquals('Undefined page', $response->getBody()->getContents());
     }
-
 }
