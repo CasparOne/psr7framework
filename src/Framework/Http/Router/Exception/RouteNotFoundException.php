ccdbed0130eae7919cfcf719f077f2c900e98eb1
CasparOne <vvolodin.victor@gmail.com>
2019-08-23 12:58:38 +0300

    Created aura/router adapter to use with our project Fix RouteNotFoundException to represents full stack-trace our router

--- src/Framework/Http/Router/Exception/RouteNotFoundException.php
+++ src/Framework/Http/Router/Exception/RouteNotFoundException.php
@@ -17,10 +17,11 @@ class RouteNotFoundException extends \LogicException
      * RouteNotFoundException constructor.
      * @param $name
      * @param array $params
+     * @param Throwable|null $previous
      */
-    public function __construct($name, array $params)
+    public function __construct($name, array $params, \Throwable $previous = null)
     {
-        parent::__construct('Route "' . $name . '" not found');
+        parent::__construct('Route "' . $name . '" not found', 0, $previous);
         $this->name = $name;
         $this->params = $params;
     }
