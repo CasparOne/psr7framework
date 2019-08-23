30ec6a914fa4015e4e94015683d9ac3c888dd63e
CasparOne <vvolodin.victor@gmail.com>
2019-08-23 11:16:27 +0300

    Add Resolver to get Actions

---
+++ src/Framework/Http/ActionResolver.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Framework\Http;
+
+
+use function is_string;
+
+class ActionResolver
+{
+    public function resolve($handler) : callable
+    {
+        return is_string($handler) ? new $handler : $handler;
+    }
+
+}
