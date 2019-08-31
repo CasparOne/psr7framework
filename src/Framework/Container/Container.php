8620c62f498751ead2a0585e2c39633fb1d6ba9b
CasparOne <vvolodin.victor@gmail.com>
2019-08-31 12:46:59 +0300

    Added ArrayAccess to the Container class

--- src/Framework/Container/Container.php
+++ src/Framework/Container/Container.php
@@ -6,7 +6,7 @@ namespace Framework\Container;
  * Class Container
  * @package Framework\Container
  */
-class Container
+class Container implements \ArrayAccess
 {
     private $definitions = [];
     private $results = [];
@@ -43,5 +43,67 @@ class Container
             unset($this->results[$id]);
         }
         $this->definitions[$id] = $value;
+    }
+
+    /**
+     * Whether a offset exists
+     * @link https://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset) : bool
+    {
+        return array_key_exists($offset, $this->definitions);
+    }
+
+    /**
+     * Offset to retrieve
+     * @link https://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * Offset to set
+     * @link https://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value) : void
+    {
+        $this->set($offset, $value);
+    }
+
+    /**
+     * Offset to unset
+     * @link https://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset) : void
+    {
+        unset($this->definitions[$offset]);
     }
 }
